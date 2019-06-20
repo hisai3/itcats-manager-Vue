037a0b8a230a8ab8dc7953577d71011f13dc9487
gitname <920928397@qq.com>
2019-06-20 18:55:43 +0800

    登录跳转，添加首页组件和路由

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/components/login'
+import Index from '@/components/index'
 Vue.use(VueRouter)
 
 var router = new VueRouter({
@@ -9,6 +10,11 @@ var router = new VueRouter({
       name: 'Login',
       path: '/login',
       component: Login
+    },
+    {
+      name: 'index',
+      path: '/index',
+      component: Index
     }
   ]
 })
